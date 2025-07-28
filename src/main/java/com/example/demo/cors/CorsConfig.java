5e472a3225a1c4996987d472441303b0a2573391
Venkata Mukesh <Venkata.mukhesh@lotuswireless.com>
2025-07-28 11:18:08 +0530

    added cors config

---
+++ src/main/java/com/example/demo/cors/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.example.demo.cors;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://mukhesh4416.github.io")  // your frontend URL
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
