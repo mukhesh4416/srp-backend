9fbb30fa610e4faf33def9a80b6315e37926ca20
Venkata Mukesh <Venkata.mukhesh@lotuswireless.com>
2025-07-28 12:20:11 +0530

    cors

--- src/main/java/com/example/demo/cors/CorsConfig.java
+++ src/main/java/com/example/demo/cors/CorsConfig.java
@@ -14,9 +14,9 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+//                        .allowedOrigins("http://localhost:5173")
 //                        .allowedOrigins("http://192.168.77.247:5173")
-//                        .allowedOrigins("https://mukhesh4416.github.io")
+                        .allowedOrigins("https://mukhesh4416.github.io")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowCredentials(true);
             }
